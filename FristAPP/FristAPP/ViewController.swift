88a57bdc0206ef7e6d2dc750353d8a1306a5ed3a
丁家财 <dingjiacai@dingjiacaideMacBook-Pro.local>
2015-12-06 16:06:55 +0800

    ??

---
+++ FristAPP/FristAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FristAPP
+//
+//  Created by 丁家财 on 15/12/4.
+//  Copyright © 2015年 kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
